3dade12fef024a64c7ba76340d11875ea63004c1
Vfjfhdhdj <thanhdanuminh020220241@gmail.com>
2026-02-28 23:03:30 +0700

    Create hubv1.lua

---
+++ hubv1.lua
@@ -0,0 +1,7 @@
+-- this script created at https://lambloxfruitscript.vercel.app/
+getgenv().ScriptTitle = "Script Title"
+getgenv().ScriptSubTitle = "bản script V1"
+getgenv().ScriptImage = "https://i.ibb.co/dJGT2Z7C/Screenshot-2026-02-19-10-53-53-39-f69139cffc4d135a71392e13634f144a.jpg"
+getgenv().ScriptAuthorName = "yutakjin"
+getgenv().ScriptAuthorSubTitle = "Author Info"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hoannhatz/Script/refs/heads/main/EZScript.lua"))()
